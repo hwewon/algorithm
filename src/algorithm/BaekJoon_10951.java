024388878b42c3069a2ca1baffa07ba9204d7232
hwewon <qkd7835@gmail.com>
2023-05-23 23:59:52 +0900

    algorithm commit

---
+++ src/algorithm/BaekJoon_10951.java
@@ -0,0 +1,25 @@
+package algorithm;
+// 백준 10951번 - 새싹
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+
+        while((str = br.readLine()) != null){
+            st = new StringTokenizer(str, " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(a+b);
+        }
+
+    }
+}
---
+++ src/algorithm/BaekJoon_2739.java
@@ -0,0 +1,20 @@
+package algorithm;
+// 백준 2739번 - 새싹
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon_2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<1; i++){
+            for(int j=1; j<10; j++){
+                System.out.printf(a + " * " + j + " = " + a*j + "%n");
+            }
+        }
+    }
+}
