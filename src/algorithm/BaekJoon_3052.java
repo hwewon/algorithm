dfc61eea9222e0098f56dd1727261e216ca69c10
hwewon <qkd7835@gmail.com>
2023-06-18 23:39:08 +0900

    algorithm commit

---
+++ src/algorithm/BaekJoon_3052.java
@@ -0,0 +1,32 @@
+package algorithm;
+// 백준 3052번 - 브론즈2
+
+
+import java.io.*;
+import java.util.*;
+
+public class BaekJoon_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+        int count = 0;
+        int j;
+
+        for(int i=0; i<10; i++){
+            int a = Integer.parseInt(br.readLine());
+
+            arr[i] += a%42;
+            for(j=0; j<10; j++){
+                if(arr[i] == arr[j]){
+                    break;
+                }
+            }
+            if(i == j){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
